07023784438f1d383b936240fc22244a078690b0
photography-ankith <ankithphotographymail@gmail.com>
2022-07-13 00:40:06 +0530

    Added Lazy Load in file test.js and included Image id as Imgxxx

---
+++ test.js
@@ -0,0 +1,116 @@
+ let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.10
+    };
+        
+let callback = (entries, observer)=>{
+    entries.forEach(entry => {
+            if(entry.isIntersecting && entry.target.className === 'gallery-img'){
+                let imageUrl = entry.target.getAttribute('data-src');
+                if(imageUrl){
+                    entry.target.src = imageUrl;
+                }
+            }
+        });
+    }
+        
+let observer = new IntersectionObserver(callback, options)
+observer.observe(document.querySelector('#Img001'));
+observer.observe(document.querySelector('#Img002'));
+observer.observe(document.querySelector('#Img003'));
+observer.observe(document.querySelector('#Img004'));
+observer.observe(document.querySelector('#Img005'));
+observer.observe(document.querySelector('#Img006'));
+observer.observe(document.querySelector('#Img007'));
+observer.observe(document.querySelector('#Img008'));
+observer.observe(document.querySelector('#Img009'));
+observer.observe(document.querySelector('#Img010'));
+observer.observe(document.querySelector('#Img011'));
+observer.observe(document.querySelector('#Img012'));
+observer.observe(document.querySelector('#Img013'));
+observer.observe(document.querySelector('#Img014'));
+observer.observe(document.querySelector('#Img015'));
+observer.observe(document.querySelector('#Img016'));
+observer.observe(document.querySelector('#Img017'));
+observer.observe(document.querySelector('#Img018'));
+observer.observe(document.querySelector('#Img019'));
+observer.observe(document.querySelector('#Img020'));
+observer.observe(document.querySelector('#Img021'));
+observer.observe(document.querySelector('#Img022'));
+observer.observe(document.querySelector('#Img023'));
+observer.observe(document.querySelector('#Img024'));
+observer.observe(document.querySelector('#Img025'));
+observer.observe(document.querySelector('#Img026'));
+observer.observe(document.querySelector('#Img027'));
+observer.observe(document.querySelector('#Img028'));
+observer.observe(document.querySelector('#Img029'));
+observer.observe(document.querySelector('#Img030'));
+observer.observe(document.querySelector('#Img031'));
+observer.observe(document.querySelector('#Img032'));
+observer.observe(document.querySelector('#Img033'));
+observer.observe(document.querySelector('#Img034'));
+observer.observe(document.querySelector('#Img035'));
+observer.observe(document.querySelector('#Img036'));
+observer.observe(document.querySelector('#Img037'));
+observer.observe(document.querySelector('#Img038'));
+observer.observe(document.querySelector('#Img039'));
+observer.observe(document.querySelector('#Img040'));
+observer.observe(document.querySelector('#Img041'));
+observer.observe(document.querySelector('#Img042'));
+observer.observe(document.querySelector('#Img043'));
+observer.observe(document.querySelector('#Img044'));
+observer.observe(document.querySelector('#Img045'));
+observer.observe(document.querySelector('#Img046'));
+observer.observe(document.querySelector('#Img047'));
+observer.observe(document.querySelector('#Img048'));
+observer.observe(document.querySelector('#Img049'));
+observer.observe(document.querySelector('#Img050'));
+observer.observe(document.querySelector('#Img051'));
+observer.observe(document.querySelector('#Img052'));
+observer.observe(document.querySelector('#Img053'));
+observer.observe(document.querySelector('#Img054'));
+observer.observe(document.querySelector('#Img055'));
+observer.observe(document.querySelector('#Img056'));
+observer.observe(document.querySelector('#Img057'));
+observer.observe(document.querySelector('#Img058'));
+observer.observe(document.querySelector('#Img059'));
+observer.observe(document.querySelector('#Img060'));
+observer.observe(document.querySelector('#Img061'));
+observer.observe(document.querySelector('#Img062'));
+observer.observe(document.querySelector('#Img063'));
+observer.observe(document.querySelector('#Img064'));
+observer.observe(document.querySelector('#Img065'));
+observer.observe(document.querySelector('#Img066'));
+observer.observe(document.querySelector('#Img067'));
+observer.observe(document.querySelector('#Img068'));
+observer.observe(document.querySelector('#Img069'));
+observer.observe(document.querySelector('#Img070'));
+observer.observe(document.querySelector('#Img071'));
+observer.observe(document.querySelector('#Img072'));
+observer.observe(document.querySelector('#Img073'));
+observer.observe(document.querySelector('#Img074'));
+observer.observe(document.querySelector('#Img075'));
+observer.observe(document.querySelector('#Img076'));
+observer.observe(document.querySelector('#Img077'));
+observer.observe(document.querySelector('#Img078'));
+observer.observe(document.querySelector('#Img079'));
+observer.observe(document.querySelector('#Img080'));
+observer.observe(document.querySelector('#Img081'));
+observer.observe(document.querySelector('#Img082'));
+observer.observe(document.querySelector('#Img083'));
+observer.observe(document.querySelector('#Img084'));
+observer.observe(document.querySelector('#Img085'));
+observer.observe(document.querySelector('#Img086'));
+observer.observe(document.querySelector('#Img087'));
+observer.observe(document.querySelector('#Img088'));
+observer.observe(document.querySelector('#Img089'));
+observer.observe(document.querySelector('#Img090'));
+observer.observe(document.querySelector('#Img091'));
+observer.observe(document.querySelector('#Img092'));
+observer.observe(document.querySelector('#Img093'));
+observer.observe(document.querySelector('#Img094'));
+observer.observe(document.querySelector('#Img095'));
+observer.observe(document.querySelector('#Img096'));
+observer.observe(document.querySelector('#Img097'));
+observer.observe(document.querySelector('#Img098'));
